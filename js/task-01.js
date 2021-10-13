28dafd4acc48399e530182bcef2417447751abba
Iraorel <79316590+Iraorel@users.noreply.github.com>
2021-10-13 21:23:21 +0300

    task1

---
+++ js/task-01.js
@@ -0,0 +1,13 @@
+
+const categoriesEL = document.querySelector('#categories');
+console.log('Number of categories:', categoriesEL);
+
+const category = document.querySelector('.item ');
+console.log('Category:', category);
+
+const categoryName = document.querySelector('.item .h2');
+console.log('categoryName', categoryName);
+console.log(categoryName.textContent);
+
+const element = document.querySelector('li');
+console.log('Elements:', element);
